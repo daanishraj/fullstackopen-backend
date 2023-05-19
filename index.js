653d02f177a527279f197dc2c04573942346fd6e
Daanish Raj <daanishraj@gmail.com>
2023-05-19 12:52:06 +0200

    3.19* Phonebook database step7

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ const errorHandler = (error, request, response, next ) => {
 
   if (error.name=== 'CastError') {
     return response.status(400).send({error: 'malformatted id'})
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({error: error.message})
   }
 
   next(error)
@@ -40,7 +42,7 @@ app.get('/api/persons', (request, response)=>{
     
 })
 
-app.post('/api/persons', (request, response)=>{
+app.post('/api/persons', (request, response, next)=>{
   const {name, number} = request.body
 
   if (!name.trim()){
@@ -57,10 +59,11 @@ app.post('/api/persons', (request, response)=>{
     number
   })
 
-    person.save().then(savedPerson=> {
+    person.save()
+    .then(savedPerson=> {
     console.log(`added ${savedPerson.name} number ${savedPerson.number} to phone book`)
     response.json(savedPerson)
-})
+}).catch(error => next(error))
   
 })
 
@@ -87,7 +90,7 @@ app.put('/api/persons/:id', (request, response, next)=>{
   const {name, number}= request.body
   const person = {name, number}
  
-  Person.findByIdAndUpdate(request.params.id, person, {new:true})
+  Person.findByIdAndUpdate(request.params.id, person, {new:true, runValidators: true, context: 'query'})
   .then(updatedPerson => {
     response.status(200).json(updatedPerson)
   })
